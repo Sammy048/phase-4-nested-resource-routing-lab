076193091019572d1a59c7f4ac5b5229e8e467e1
sammy-coder <sammymasib000@gmail.com>
2022-11-30 05:33:51 +0300

    Completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
-  resources :users, only: [:show]
+  
+  resources :users, only: [:show] do
+    resources :items
+  end
+
+  get "/items", to: "items#index"
+
 end
